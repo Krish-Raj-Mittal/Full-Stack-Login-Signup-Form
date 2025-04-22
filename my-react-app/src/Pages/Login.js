e753bae4a6ae48fed6d70249f751ef8e7f2890a4
Krish-Raj-Mittal <krishrajmittal@gmail.com>
2025-04-22 13:09:01 +0530

    Update Login.js
    changed backend api

--- my-react-app/src/Pages/Login.js
+++ my-react-app/src/Pages/Login.js
@@ -24,7 +24,7 @@ const Login = () => {
         e.preventDefault();
 
         try{
-            await axios.post('https://full-stack-login-signup-form-2.onrender.com/api/v1/login', formData)
+            await axios.post('https://full-stack-login-signup-form-3.onrender.com', formData)
             alert('User Logged In');
         }
         catch(err){
